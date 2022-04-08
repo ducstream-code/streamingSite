90c6799c6d3ebb9f2ed7616b523608d9779b864b
ducstream-code <aurelienkrief@gmail.com>
2022-04-09 00:07:14 +0200

    front page video

---
+++ js/video.js
@@ -0,0 +1,44 @@
+function removeHUD(){
+    let video = document.getElementById('video')
+    video.controls = false;
+}
+
+function play() {
+    let video = document.getElementById('video')
+    video.play();
+    let button = document.getElementById('pause')
+    button.setAttribute("onclick", "pause()");
+    button.innerHTML='<ion-icon name="pause"></ion-icon>'
+}
+
+function pause(){
+    let video = document.getElementById('video')
+    video.pause();
+    let button = document.getElementById('pause')
+    button.setAttribute("onclick", "play()");
+    button.innerHTML="<ion-icon name=\"play\"></ion-icon>"
+
+}
+
+function minusTime(time){
+    let video = document.getElementById('video')
+    video.currentTime-=time;
+
+}
+
+function plusTime(time){
+    let video = document.getElementById('video')
+    video.currentTime+=time;
+
+}
+
+function goFullScreen(){
+    let elem = document.getElementById('video')
+    if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+    } else if (elem.webkitRequestFullscreen) { /* Safari */
+        elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) { /* IE11 */
+        elem.msRequestFullscreen();
+    }
+}

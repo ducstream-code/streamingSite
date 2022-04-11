04c49e2445cc9d2d3ba02902a73e7ee01d855733
ducstream-code <aurelienkrief@gmail.com>
2022-04-11 15:25:05 +0200

    test page

---
+++ includes/mobileHeader.php
@@ -0,0 +1,11 @@
+<?php
+?>
+<div class="responsive_header h-12 p-4 w-full bg-[#262633] mb-4 flex justify-between place-items-center md:hidden">
+    <ion-icon class="text-3xl text-[#70708F]" name="menu"></ion-icon>
+    <h1 class="text-xl font-semibold text-[#70708F]">NomDuSite</h1>
+    <div class="flex w-2/12 justify-between">
+        <img class="object-contain" src="../assets/images/icons/Search.png">
+        <img class="object-contain h-8 rounded-3xl" src="../assets/images/placeholder/user.jpg">
+
+    </div>
+</div>
--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,6 +1,6 @@
 <?php
 ?>
-<div class="side_bar h-full md:w-0  lg:w-20 flex bg-[#262633] flex-col place-items-center pt-[10px] fixed hidden lg:flex  ">
+<div class="side_bar h-full w-0  md:w-20 flex bg-[#262633] flex-col place-items-center pt-[10px] fixed hidden md:flex  ">
     <img src="../assets/images/icons/ic.png" class="mb-[35px] w-8/12">
     <img src="../assets/images/icons/logo.png" class="mb-[82px] h-12">
     <img src="../assets/images/icons/home.png" class="mb-[34px] h-12">

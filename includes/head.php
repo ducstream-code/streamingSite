99626ae897cbe5a833f8f74b157dcb9333343178
ducstream-code <aurelienkrief@gmail.com>
2022-04-12 18:43:24 +0200

    front end actors, categories, search, index (*responsive)

--- includes/head.php
+++ includes/head.php
@@ -7,6 +7,7 @@
 <script src="https://cdn.jsdelivr.net/npm/fullcalendar@5.10.2/main.min.js"></script>
 <link rel="stylesheet" href="../css/main.css">
 <script src="../externals/tailwind.js"></script>
+<script src="https://code.iconify.design/2/2.1.0/iconify.min.js"></script>
 <meta charset="utf-8">
 <meta name="viewport" content="width-device-width, initial-scale=1">
 <script>
@@ -17,7 +18,7 @@
                     background: '#da373d',
                 }
                 screens: {
-                    '2sm': '1600px',
+                    '2xl': '1900px',
                 },
             }
         }
--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,12 +1,13 @@
 <?php
 ?>
-<div class="side_bar h-full w-0  md:w-20 flex bg-[#262633] flex-col place-items-center pt-[10px] fixed hidden md:flex  ">
-    <img src="../assets/images/icons/ic.png" class="mb-[35px] w-8/12">
-    <img src="../assets/images/icons/logo.png" class="mb-[82px] h-12">
-    <img src="../assets/images/icons/home.png" class="mb-[34px] h-12">
-    <img src="../assets/images/icons/tranding.png" class="mb-[34px] h-12">
-    <img src="../assets/images/icons/explorer.png" class="mb-[34px] h-12">
-    <img src="../assets/images/icons/liked.png" class="mb-[34px] h-12" >
-    <img src="../assets/images/icons/history.png" class="mb-[34px] h-12">
+<div class="side_bar h-full w-0  md:w-20 flex bg-[#262633] flex-col place-items-center pt-[10px] fixed hidden md:flex top-0  ">
+   <a href="#"> <img src="../assets/images/icons/ic.png" class="mb-[35px] w-9/12 cursor-pointer"></a>
+    <a href="#"> <img src="../assets/images/icons/logo.png" class="mb-[82px] h-12"></a>
+    <a href="/index.php"> <img src="../assets/images/icons/home.png" class="mb-[34px] h-12 cursor-pointer"></a>
+    <a href="#"> <img src="../assets/images/icons/tranding.png" class="mb-[34px] h-12"></a>
+    <a href="#"> <img src="../assets/images/icons/explorer.png" class="mb-[34px] h-12"></a>
+    <a href="#"> <img src="../assets/images/icons/explorer.png" class="mb-[34px] h-12"></a>
+    <a href="#"> <img src="../assets/images/icons/liked.png" class="mb-[34px] h-12" ></a>
+    <a href="#"> <img src="../assets/images/icons/history.png" class="mb-[34px] h-12"></a>
 
 </div>

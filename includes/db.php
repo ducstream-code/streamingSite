d05eeabe7e8ef23337bc4adc049ebbf563aaa578
ducstream-code <73170287+ducstream-code@users.noreply.github.com>
2022-05-01 13:45:15 +0200

    Delete db.php

--- includes/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-
-//connexion a la base de donnée
-try {
-    $db = new PDO('mysql:host=152.228.218.3:3306;dbname=streaming', 'rooters', 'P?ok68y8', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
-} catch (Exception $e) {
-    die('Erreur : ' . $e->getMessage());
-}
